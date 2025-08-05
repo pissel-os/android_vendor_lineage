4f0f03435eaf58c9f395425b5feba8bffebf4d85
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-05 13:49:12 +0530

    vendor: config: Exclude Audio on custom builds
    Change-Id: I094e1cb584fc31cf510b96c1e4dbcd7240b3fad3

--- config/telephony.mk
+++ config/telephony.mk
@@ -17,8 +17,10 @@ PRODUCT_PACKAGES += \
     Stk
 
 # Default ringtone
+ifeq ($(LINEAGE_BUILD),true)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Orion.ogg
+endif
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
